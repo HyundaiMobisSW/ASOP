f443c7146e319d074633bd523ce6af736f1e4356
khk1262 <khk1262@gmail.com>
2022-09-29 19:50:40 +0900

    0929

--- KHK1262/6_WEEK/boj_2812.cpp
+++ KHK1262/6_WEEK/boj_2812.cpp
@@ -21,24 +21,21 @@ int main() {
 
 		int t = s[j] - '0';
 
-		if (j == 0) st.push(t); // 처음은 그냥 push
-
-		else {
-			if (cnt < k) {
-				while (!st.empty() && st.top() < t) {
-					if (cnt == k) break; // cnt 다 차면 break
-					cnt++;
-					st.pop();
-				}
+		if (cnt < k) {
+			while (!st.empty() && st.top() < t) {
+				if (cnt == k) break; // cnt 다 차면 break
+				cnt++;
+				st.pop();
 			}
-			st.push(t);
 		}
+		st.push(t);
 	}
+
 	while (!st.empty()) { // 현재 stack 안에 순서가 반대로 들어있으므로
 		v.push_back(st.top());
 		st.pop();
 	}
 
-	for (int i = v.size() - 1; i >= k - cnt; i--)
+	for (int i = v.size() - 1; i >= k - cnt; i--) // 아직 남은 카운트만큼 뒷자리 자름
 		cout << v[i];
 }
